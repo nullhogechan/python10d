fb8865006d371ac77cd9215ed602eee742d6e580
sho kobayashi <ibsims297@gmail.com>
2015-11-03 18:45:07 +0900

    条件式と不等号の練習

--- 条件分岐.py
+++ 条件分岐.py
@@ -7,3 +7,26 @@ elif a==1:
     print("one")
 else:
     print("many")
+
+b=1
+if b>0:
+    print("Positive")
+elif b==1:
+    print("one")
+
+    
+c=1
+print(c==1)
+print(c!=1)
+print(c>0)
+print(c>2)
+print(c>=1)
+print(c<=1)
+print(not c==2)
+
+d=1
+e=5
+print(d<2 and e>3)
+print(d<2 and e<3)
+print(d<2 or e<3)
+print(d>2 or e<3)
